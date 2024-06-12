4c41390c45ab0d435ed4bd1be50274cc83cafe18
amaan8429 <mohdamaan8429@gmail.com>
2024-06-12 12:56:35 +0530

    removed commander

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 #! /usr/bin/env node
 
 import path from "path";
-import { Command } from "commander";
 import { fileExists } from "./functions";
 import {
   handlePublicFolder,
@@ -13,24 +12,6 @@ import {
 } from "./handles";
 
 const currentDir: string = process.cwd();
-
-const program = new Command();
-
-program
-  .name("next-clean")
-  .description("Clean and reset the boilerplate code in a Next.js project")
-  .version("1.0.0")
-  .helpOption("-h, --help", "Display help for command")
-  .addHelpText(
-    "after",
-    `
-  GitHub: https://github.com/amaan8429/next-clean
-  
-  next-clean is a command-line tool designed to clean and reset the boilerplate code in a Next.js project. It aims to provide a fresh starting point for developers who want to work on a clean Next.js codebase without any unnecessary files or configurations.
-  `
-  );
-
-program.parse(process.argv);
 
 async function main() {
   const publicPath: string = path.join(currentDir, "public");
